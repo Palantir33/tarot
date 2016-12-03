dea3dde0862dae8da9976d511c70b132f5917942
Tewan <tewan33@gmail.com>
2016-12-03 16:18:16 +0100

    Affiche écran vert sans cartes (pour l'instant)

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,26 +1,35 @@
 package application;
-	
+
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane; 
+import javafx.scene.paint.Color; 
+import javafx.scene.shape.Rectangle; 
+import javafx.stage.Stage;
 
 
-public class Main extends Application {
+
+public class Main extends Application{
+	
+	private Modele modele;
+	private Vue vue;
+	
+
 	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+	public void start(Stage fenetre) throws Exception {
+		// TODO Auto-generated method stub
+		
+		this.modele = new Modele();
+		this.vue = new Vue(this.modele);
+		modele.setVue(vue);
+		
 	}
 	
-	public static void main(String[] args) {
-		launch(args);
-	}
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+
 }
