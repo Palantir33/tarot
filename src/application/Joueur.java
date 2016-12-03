cb92f08ad92cb9e43bcf7717138e1a61703db122
Tewan <tewan33@gmail.com>
2016-12-03 16:17:26 +0100

    Classe Joueur terminée (normalement)

--- src/application/Joueur.java
+++ src/application/Joueur.java
@@ -1,5 +1,24 @@
 package application;
 
-public class Joueur {
+import java.util.ArrayList;
 
+public class Joueur {
+	
+	public ArrayList<Carte> cartes = new ArrayList<Carte>();
+	
+	public Joueur(){
+		
+	}
+	
+	public void ajouterCartesaJoueur(Carte c){
+		cartes.add(c);
+	}
+	
+	public void supprimerCartesaJoueur(Carte c){
+		cartes.remove(c);
+	}
+	
+	public ArrayList<Carte> getCartes(){
+		return cartes;
+	}
 }
