ab53573fa3ca4a50c32c0a15c5630bee17a68ed4
Tewan <tewan33@gmail.com>
2016-12-03 16:15:53 +0100

    getter/setter

--- src/application/Carte.java
+++ src/application/Carte.java
@@ -7,12 +7,25 @@ public class Carte {
 	private int y;
 	private boolean dansChien;
 	
-	public Carte() {
+	public Carte(int x, int y, ImageCarte i) {
 		this.i = i;
 		this.x = x;
 		this.y = y;
 		dansChien = false;
-		
-	}
 
+	}
+	
+	public void setX(int x){
+		this.x = x;
+		i.setX(x);
+	}
+	
+	public void setY(int y){
+		this.y = y;
+		i.setY(y);
+	}
+	
+	public ImageCarte getImage(int i){
+		return this.i;
+	}
 }
