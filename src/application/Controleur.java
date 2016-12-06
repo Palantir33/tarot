6f440de73f8897d6967100128f9f75a9df61b4ad
Romaing <rom.cremery@gmail.com>
2016-12-06 20:03:27 +0100

    rien

--- src/application/Controleur.java
+++ src/application/Controleur.java
@@ -4,8 +4,14 @@ public class Controleur {
 	public Modele modele;
 	public Vue vue;
 	
-	public Controleur(Modele m, Vue v){
+	public Controleur(Modele m, Vue v)
+	{
 		this.modele = m;
 		this.vue = v;
+	}
+	
+	public void setControleur(Modele m)
+	{
+		this.modele = m;
 	}
 }
