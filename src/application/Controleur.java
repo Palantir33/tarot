b5545e3cc54899d8dc28bc984c96e582862f4619
Tewan <tewan33@gmail.com>
2016-12-08 23:44:42 +0100

    Déplacement des actions dans Vue vers Controleur

--- src/application/Controleur.java
+++ src/application/Controleur.java
@@ -1,5 +1,8 @@
 package application;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
 public class Controleur {
 	public Modele modele;
 	public Vue vue;
@@ -8,6 +11,19 @@ public class Controleur {
 	{
 		this.modele = m;
 		this.vue = v;
+		
+		vue.getQuitter().setOnAction(new EventHandler<ActionEvent>(){
+			@Override
+			public void handle(ActionEvent event){
+				System.exit(0);
+			}
+		});
+		vue.getDistribution().setOnAction(new EventHandler<ActionEvent>(){
+			@Override
+			public void handle(ActionEvent event){
+				//Faire distribution
+			}
+		});
 	}
 	
 	public void setControleur(Modele m)
